94220c8e65644650fc07ee11d178442e2dac556d
wangxin <xinwang213z@gmail.com>
2022-03-12 05:41:16 +0000

    sumArray

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (array.length > 0) {
+    let item = array.shift();
+    if (Array.isArray(item)) {
+      return sumItems(item) + sumItems(array);
+    }
+    return item + sumItems(array);
+  }
+  return 0;
 }
 
 module.exports = sumItems;
